7248c609e3816022a833fc3837c5d147f5e15e6d
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2025-04-30 19:38:05 +0530

    add prettier formatting

---
+++ prettier.config.js
@@ -0,0 +1,35 @@
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "none",
+  semi: true,
+  importOrder: [
+    "^(react/(.*)$)|^(react$)|^(react-dom$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/pages/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/styles/(.*)$",
+    "",
+    "^[./]",
+    "",
+    "<TYPES>^(node:)",
+    "<TYPES>",
+    "<TYPES>^@/types(?:/(.*))?$",
+    "<TYPES>^[.]"
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: false,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"]
+};
+
+export default config;
