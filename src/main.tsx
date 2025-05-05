fdd5fcd45a9d7a7f1198e3e6b4a03d8931af7e28
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2025-05-05 18:37:21 +0530

    add file path aliasing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./styles/global.css";
+import "@/styles/global.css";
 
-import App from "./App.tsx";
+import App from "@/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
